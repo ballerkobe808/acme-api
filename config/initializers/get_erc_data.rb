08f4f02f19c46789110bcb5eacdb0aa8bfd09885
danielkong <daniel_kong@datahouse.com>
2018-05-16 11:41:25 -1000

    stopping refresh

--- config/initializers/get_erc_data.rb
+++ config/initializers/get_erc_data.rb
@@ -6,7 +6,7 @@ Thread.new do
 
   # perform a refresh of the coins erc20 every 4 hours
   while true do
-    GetErcDataJob.perform_now
+    # GetErcDataJob.perform_now
     sleep(4.hours)
   end
     
